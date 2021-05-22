1c8c4e123e53bb48401a35cc16047022cd7b235f
Nitish Agrahari <nitish.agrahari@eazydiner.com>
2021-05-22 17:06:05 +0530

    feat: Footer component added
    Footer component added for the github navigation link

---
+++ src/components/Footer/Footer.tsx
@@ -0,0 +1,18 @@
+import React from 'react';
+import './Footer.css';
+
+const Footer = () => {
+  return (
+    <div className="footer-container">
+        <a 
+          href="https://github.com/nitagr"
+          className="footer-link"
+          target="_blank"
+          rel="noreferrer"> 
+        Get code on Github
+        </a>
+    </div>
+  );
+}
+
+export default Footer;
